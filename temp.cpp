22d58214c4cd8296b700c572d0e7417af88c2d9d
Vrushank G <93258530+vrushank41@users.noreply.github.com>
2023-02-17 14:19:00 +0530

    Final change made

--- temp.cpp
+++ temp.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, I am Vrushank G of SEC-I\n";
   cout << "I am in Task5 and my SRN is PES1UG20CS516\n";
-  cout << "Ingested an error to display failure;
+  cout << "Resolved the error";
   return 0;
 }
